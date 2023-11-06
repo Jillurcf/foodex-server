2180e1b3fb06f73584e7880c8fabe7a84ddd0ce8
jillurcf <jrtushar@gmail.comm>
2023-11-06 08:35:55 -0800

    deploy with vercel success

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion} = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+
+// Enable CORS for all routes
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://assignment11-5c40c.web.app'); // Replace with the correct origin
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "https://assignment11-5c40c.web.app",
-      "https://assignment11-5c40c.firebaseapp.com"
-     
+      "https://assignment11-5c40c.firebaseapp.com",
     ],
     credentials: true,
   })
@@ -21,10 +30,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfumfky.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,97 +38,98 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-
 // middleware created by me
-const logger = async (req, res, next) => {
-  console.log("called", req.host, req.originalUrl);
-  next();
-};
+// const logger = async (req, res, next) => {
+//   console.log("called", req.host, req.originalUrl);
+//   next();
+// };
 
-const verifyToken = async(req, res, next)=>{
-  const token = req?.cookies?.token;
-  if(!token){
-    return res.status(401).send({message: 'unauthorized access'})
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, docoded)=>{
-    if(err){
-      return res.status(401).send({message: 'unauthorized access' })
-    }
-    req.user = docoded;
-    next()
-  })
-}
+// const verifyToken = async (req, res, next) => {
+//   const token = req?.cookies?.token;
+//   if (!token) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, docoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
+//     req.user = docoded;
+//     next();
+//   });
+// };
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const allFoodCollection = client.db("allFoodDB").collection("allFood");
     const userCollection = client.db("userDB").collection("user");
-    
+
     // AllFood Related Api
-    app.get('/api/v1/allFood', async(req, res)=>{
-    
+    app.get("/api/v1/allFood", async (req, res) => {
       const page = parseInt(req.query.page);
-      const size = parseInt(req.query.size)
-      console.log('pagination', req.query);
-        const result = await allFoodCollection.find()
+      const size = parseInt(req.query.size);
+      console.log("pagination", req.query);
+      const result = await allFoodCollection
+        .find()
         .skip(page * size)
         .limit(size)
         .toArray();
-        res.send(result);
-    })
+      res.send(result);
+    });
 
     // Pagination related api
-       app.get('/api/v1/allFoodCount', async(req, res)=>{
+    app.get("/api/v1/allFoodCount", async (req, res) => {
       const count = await allFoodCollection.estimatedDocumentCount();
-      res.send({count}); 
-      console.log({count});
-    })
+      res.send({ count });
+      console.log({ count });
+    });
     // user related api
 
-    app.post('/api/v1/user', async(req, res)=>{
+    app.post("/api/v1/user", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
-      res.send(result)
+      res.send(result);
       console.log(result);
+    });
 
-    })
+    // auth related api
 
-     // auth related api
-
-     app.post('/api/v1/jwt', async(req, res) =>{
+    app.post("/api/v1/jwt", async (req, res) => {
       const user = req.body;
-      console.log('user for token', user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        
-    })
-      .send({success: true})
-    })
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
-
-    
-    app.post('/api/v1/loggedOut', async(req, res)=>{
+    app.post("/api/v1/loggedOut", async (req, res) => {
       const user = req.body;
-      console.log('logged out', user);
-      res.clearCookie('token', {maxAge: 0, 
-        secure: process.env.NODE_ENV === 'production', 
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      }).send({success: true})
-
-    })
+      console.log("logged out", user);
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -130,14 +137,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res)=>{
-    res.send('assignment11 is running')
-})
+app.get("/", (req, res) => {
+  res.send("assignment11 is running");
+});
 
 app.listen(port, () => {
-    console.log(`assignment11 server is running ${port}`);
-  });
-  
+  console.log(`assignment11 server is running ${port}`);
+});
