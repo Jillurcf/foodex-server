33524f51c11a428b4811b004aaf71a86691fe51a
jillurcf <jrtushar@gmail.comm>
2023-11-05 21:17:56 -0800

    fake json data added to the Mongodeb

--- index.js
+++ index.js
@@ -38,12 +38,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const allFoodCollection = client.db("allFoodDB").collection("allFood");
+    const userCollection = client.db("userDB").collection("user");
+    
+
+
+    // user related api
+
+    app.post('/user', async(req, res)=>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+      console.log(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
